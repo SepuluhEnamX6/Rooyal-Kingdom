3e66292f266de74a01bc62dc8b5bb62a6544f988
SepuluhEnamX6 <163085628+SepuluhEnamX6@users.noreply.github.com>
2026-08-28 22:01:32 +0700

    Delete app directory

--- app/api/auth/logout/route.js
+++
@@ -1,8 +0,0 @@
-// app/api/auth/logout/route.js
-import { NextResponse } from 'next/server';
-
-export async function POST() {
-  const response = NextResponse.json({ success: true });
-  response.cookies.delete('rk_token');
-  return response;
-}
